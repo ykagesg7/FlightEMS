e650217fffc7e94d94bdf7958b41605835bef8c6
ykagesg7 <52264421+ykagesg7@users.noreply.github.com>
2024-09-02 22:20:34 +0900

    Add files via upload

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,10 +1,6 @@
-import { createClient } from '../node_modules/@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables')
-}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
