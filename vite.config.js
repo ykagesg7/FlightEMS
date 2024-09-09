aec258830350f600d5586648118c2c4e378b56ec
ykagesg7 <52264421+ykagesg7@users.noreply.github.com>
2024-09-09 22:37:23 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,19 @@
-import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: "8080",
-  },
-  build:{
-    rollupOptions:{
-      input:{
-        main:resolve(__dirname,'index.html'),
-      }
-    },
-    target:'es2022'
-  },
-  envPrefix: 'VITE_',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "lib",
-        replacement: resolve(__dirname, "lib"),
-      },
-    ],
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
   },
 });
