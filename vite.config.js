90ee23a63d655a41207850d7c8e623bc11256211
ykagesg7 <52264421+ykagesg7@users.noreply.github.com>
2024-09-10 21:03:22 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -12,4 +13,9 @@ export default defineConfig({
       },
     },
   },
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),  // これを追加
+    },
+  },
+});
