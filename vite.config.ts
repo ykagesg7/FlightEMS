eb65b8166d7c459992c4002e5e008f70034393c2
ykagesg7 <52264421+ykagesg7@users.noreply.github.com>
2025-03-15 08:44:38 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    optimizeDeps: {
-      exclude: ['lucide-react'],
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-    assetsInclude: ['/.geojson'],
-    // Vercel向けにはルートパスではなく、相対パスを利用する
-    base: command === 'serve' ? '/' : './',
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  }
 });
